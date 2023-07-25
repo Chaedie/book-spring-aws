7cb3bd4b94cbb69538ce6339b8e9316a4785ff93
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-25 16:06:55 +0900

    Feat: HelloDto API 작성

--- src/test/java/chaedie/awsspring/web/HelloControllerTest.java
+++ src/test/java/chaedie/awsspring/web/HelloControllerTest.java
@@ -1,23 +1,15 @@
 package chaedie.awsspring.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.swing.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,14 +17,31 @@ class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
-    
+
     @Test
-    void hello가_리턴된다() throws Exception{
+    void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello))
                 .andDo(print());
+    }
+
+    @Test
+    void helloDto() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        // when
+
+        // then
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
