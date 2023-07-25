3ae2bf41d5e499f8284ed34dd35df32d4b10b404
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-25 22:44:05 +0900

    Feat: Posts update, read 추가

--- src/main/java/chaedie/awsspring/service/posts/PostsService.java
+++ src/main/java/chaedie/awsspring/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package chaedie.awsspring.service.posts;
 
+import chaedie.awsspring.domain.posts.Posts;
 import chaedie.awsspring.domain.posts.PostsRepository;
+import chaedie.awsspring.web.dto.PostsResponseDto;
 import chaedie.awsspring.web.dto.PostsSaveRequestDto;
+import chaedie.awsspring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,4 +19,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
