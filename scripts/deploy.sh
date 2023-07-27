7df7f665b0df630bde3d9ad8285c5aef12a3ec67
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-27 13:43:37 +0900

    Chore: 무중단 배포를 위한 스크립트, yml 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,12 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=aws-spring
 
 echo "> Build 파일 복사"
-
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
-
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-
 CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
 if [ -z "$CURRENT_PID" ]; then
 	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else 
@@ -24,17 +19,14 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep jar | tail -n 1)
-
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
 
+echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-real.properties \
     -Dspring.profiles.active=real \
