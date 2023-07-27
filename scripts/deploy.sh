c0f99c822e14889fc1af2e0e92009106fd9ef754
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-27 11:50:57 +0900

    Fix: Script Pid grep문 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME} | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
